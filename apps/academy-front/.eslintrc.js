4bd5ece1f1113baea74930a9ce0929bd6546da31
Hugo <hmoralesg@icloud.com>
2023-01-04 00:26:59 -0300

    fix: lint project

--- apps/academy-front/.eslintrc.js
+++ apps/academy-front/.eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  extends: ["../../.eslintrc.yml", "plugin:vue/vue3-recommended"],
-  parser: "vue-eslint-parser",
+  extends: ['../../.eslintrc.yml', 'plugin:vue/vue3-recommended'],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     project: `${__dirname}/tsconfig.eslint.json`,
-    extraFileExtensions: [".vue"],
+    extraFileExtensions: ['.vue'],
   },
   rules: {
-    "import/no-extraneous-dependencies": 0,
-    "@typescript-eslint/naming-convention": 0,
-    "import/extensions": 0,
+    'import/no-extraneous-dependencies': 0,
+    '@typescript-eslint/naming-convention': 0,
+    'import/extensions': 0,
   },
   overrides: [
     {
-      files: ["src/**/*.vue"],
+      files: ['src/**/*.vue'],
       rules: {
-        "@typescript-eslint/no-unused-vars": 0,
+        '@typescript-eslint/no-unused-vars': 0,
       },
     },
   ],
