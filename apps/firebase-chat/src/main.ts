9d4f88c82a3ade81fb0661bf06625b6a417e5dcd
Hugo <hugo@42devs.cl>
2023-01-28 14:21:05 -0300

    feat: file based router

--- apps/firebase-chat/src/main.ts
+++ apps/firebase-chat/src/main.ts
@@ -1,10 +1,20 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from '~pages';
+
 import App from './App.vue';
 
 const app = createApp(App);
 // Pinia store logic
 const pinia = createPinia();
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 app.use(pinia);
+
+app.use(router);
 
 app.mount('#app');
