1476f2ca81cf379598addb5fc04dcad7f101a3b1
Hugo <hmoralesg@icloud.com>
2023-01-29 22:05:38 -0300

    feat: layouts support

--- apps/firebase-chat/vite.config.ts
+++ apps/firebase-chat/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import Pages from 'vite-plugin-pages';
+import Layouts from 'vite-plugin-vue-layouts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Vue(),
     Pages(),
+    Layouts(),
   ],
   server: {
     port: 3000,
