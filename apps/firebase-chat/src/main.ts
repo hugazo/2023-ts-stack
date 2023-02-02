712550a74d5bb0eb6658e22f02cc10cccbeb8e99
Hugo <hmoralesg@icloud.com>
2023-02-02 18:25:24 -0300

    feat: quasar framework

--- apps/firebase-chat/src/main.ts
+++ apps/firebase-chat/src/main.ts
@@ -1,6 +1,14 @@
+// Vue main app
 import { createApp } from 'vue';
+// State management
 import { createPinia } from 'pinia';
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate';
+// Ui Library
+import { Quasar } from 'quasar';
+import quasarIconSet from 'quasar/icon-set/mdi-v6';
+import '@quasar/extras/mdi-v6/mdi-v6.css';
+import 'quasar/src/css/index.sass';
+// Router
 import { createRouter, createWebHistory } from 'vue-router';
 import { setupLayouts } from 'virtual:generated-layouts';
 import generatedRoutes from '~pages';
@@ -16,6 +24,11 @@ const routes = setupLayouts(generatedRoutes);
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+app.use(Quasar, {
+  plugins: {},
+  iconSet: quasarIconSet,
 });
 
 app.use(pinia);
