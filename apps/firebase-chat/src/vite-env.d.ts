43a4ddc2f7200f2b5f0ded4c2c9c75c339a837fb
Hugo <hmoralesg@icloud.com>
2023-02-04 16:52:32 -0300

    chore: updated env types

--- apps/firebase-chat/src/vite-env.d.ts
+++ apps/firebase-chat/src/vite-env.d.ts
@@ -1,3 +1,17 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-vue-layouts/client"/>
+
+interface ImportMetaEnv {
+  readonly FIREBASE_API_KEY: string,
+  readonly FIREBASE_AUTH_DOMAIN: string,
+  readonly FIREBASE_PROJECT_ID: string,
+  readonly FIREBASE_STORAGE_BUCKET: string,
+  readonly FIREBASE_MESSAGING_SENDER_ID: string,
+  readonly FIREBASE_APP_ID: string,
+  readonly FIREBASE_RECAPTCHA_KEY: string,
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv,
+}
