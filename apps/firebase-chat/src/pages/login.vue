7d9ea182c0ef860ae5b95a423f2d2b1e3f99155d
Hugo <hmoralesg@icloud.com>
2023-02-17 03:48:16 -0300

    feat: updated login basic layout

--- apps/firebase-chat/src/pages/login.vue
+++ apps/firebase-chat/src/pages/login.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
-.row
+q-page.window-height.window-width.row.justify-center.items-center
   .column
-    .section
-      template(v-if="authStore.user")
-        q-btn(@click.prevent="authStore.logout")
-          | Logout
-        pre {{ authStore.user }}
-      p(v-else-if="authStore.user === undefined")
-        | Loading...
-      q-btn(v-else @click.prevent="authStore.googleSignIn")
-        | Sign in with Google
+    .row.justify-center
+      h5 Welcome
+    .row
+      .card.q-pa-lg.shadow-2
+        template(v-if="authStore.user")
+          q-btn(@click.prevent="authStore.logout")
+            | Logout
+          pre {{ authStore.user }}
+        p(v-else-if="authStore.user === undefined")
+          | Loading...
+        template(v-else)
+          q-btn(@click.prevent="authStore.googleSignIn")
+            | Sign in with Google
 </template>
 
 <script setup lang="ts">
