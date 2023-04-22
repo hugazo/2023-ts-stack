577f1a765bbaf463888adf9604d98ceb085742af
Hugo <hmoralesg@icloud.com>
2023-04-21 23:43:05 -0400

    fix: updated eslint patterns

--- apps/minimal-ts-template/.eslintrc.cjs
+++ apps/minimal-ts-template/.eslintrc.cjs
@@ -2,6 +2,11 @@ module.exports = {
   extends: '../../.eslintrc.yml',
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    project: `${__dirname}/tsconfig.eslint.json`,
+    project: `${__dirname}/tsconfig.json`,
   },
+  "ignorePatterns": [
+    "dist",
+    "node_modules",
+    ".eslintrc.cjs"
+  ],
 };
