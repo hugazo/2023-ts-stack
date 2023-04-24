e673b688d58f43a0c2926b4071b6cb2380c61b0c
Hugo <hmoralesg@icloud.com>
2023-04-24 16:21:35 -0400

    [feat] Base firebase-admin connections

--- apps/firebase-admin-cli/src/index.ts
+++ apps/firebase-admin-cli/src/index.ts
@@ -1,3 +1,15 @@
+import dotenv from 'dotenv';
+import { initializeApp } from 'firebase-admin/app';
 import mainScreen from './screens/main.js';
+
+dotenv.config();
+
+if (process.env.MODE === 'development') {
+  console.log('Running in development mode');
+}
+
+const app = initializeApp();
+
+console.log(`App initialized: ${app.name}`);
 
 await mainScreen();
