4c9723370be682df0308489f6b39d8fb0a06a781
Hugo <hmoralesg@icloud.com>
2023-06-04 22:33:30 -0400

    updated eslint config

--- packages/services/.eslintrc.cjs
+++ packages/services/.eslintrc.cjs
@@ -4,15 +4,9 @@ module.exports = {
   parserOptions: {
     project: `${__dirname}/tsconfig.eslint.json`
   },
-  "include": [
-    "src/**/*.ts",
-    "src/**/*.d.ts",
-    "test/**/*.ts",
-    ".eslintrc.cjs",
-    "jest.config.js"
-  ],
-  "ignorePatterns": [
+  ignorePatterns: [
     "dist",
     "node_modules",
+    ".eslintrc.cjs",
   ],
 };
