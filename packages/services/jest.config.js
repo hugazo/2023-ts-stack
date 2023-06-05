7c8cecc5d005c1c761d6987de7a78ced37539de1
Hugo <hmoralesg@icloud.com>
2023-06-05 00:07:30 -0400

    updated jest config for esm

--- packages/services/jest.config.js
+++ packages/services/jest.config.js
@@ -1,6 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
