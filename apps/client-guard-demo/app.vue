c0ebd3ab33e5f18bba2fa12c05fb0f1f5a413929
Hugo <hmoralesg@icloud.com>
2023-06-14 01:21:11 -0400

    added monorepo library and initializing firebase app

--- apps/client-guard-demo/app.vue
+++ apps/client-guard-demo/app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>Hi</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import { initializeFirebaseApp, FirebaseInitConfig } from '@services/firebase/init';
+
+const config = useRuntimeConfig();
+
+const firebaseApp = initializeFirebaseApp(config.public as FirebaseInitConfig);
+
+console.log(firebaseApp);
+</script>
--- apps/client-guard-demo/nuxt.config.ts
+++ apps/client-guard-demo/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  runtimeConfig: {
+    // Will be available in client
+    public: {
+      mode: process.env.MODE,
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      firebaseRecaptchaKey: process.env.FIREBASE_RECAPTCHA_KEY,
+      appCheckDebugToken: process.env.FIREBASE_APPCHECK_DEBUG_TOKEN,
+    },
+  },
 })
